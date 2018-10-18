1aec2fd8a44e288696f9d3fa30ee42b155614018
HarrisonWelch <hwelch1@my.apsu.edu>
2018-10-17 23:50:48 -0500

    added a basic hand detect python program

--- hand_dectect_test.py
+++ hand_dectect_test.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import cv2
+import imutils
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -11,36 +12,129 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 # img = cv2.imread('sachin.jpg')
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# global variables
+bg = None
+
+#-------------------------------------------------------------------------------
+# Function - To find the running average over the background
+#-------------------------------------------------------------------------------
+def run_avg(image, aWeight):
+  global bg
+  # initialize the background
+  if bg is None:
+      bg = image.copy().astype("float")
+      return
+
+  # compute weighted average, accumulate it and update the background
+  cv2.accumulateWeighted(image, bg, aWeight)
+
+#-------------------------------------------------------------------------------
+# Function - To segment the region of hand in the image
+#-------------------------------------------------------------------------------
+def segment(image, threshold=25):
+    global bg
+    # find the absolute difference between background and current frame
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+
+    # threshold the diff image so that we get the foreground
+    thresholded = cv2.threshold(diff,
+                                threshold,
+                                255,
+                                cv2.THRESH_BINARY)[1]
+
+    # get the contours in the thresholded image
+    (_, cnts, _) = cv2.findContours(thresholded.copy(),
+                                    cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)
+
+    # return None, if no contours detected
+    if len(cnts) == 0:
+        return
+    else:
+        # based on contour area, get the maximum contour which is the hand
+        segmented = max(cnts, key=cv2.contourArea)
+        return (thresholded, segmented)
+
+#-------------------------------------------------------------------------------
+# Main function
+#-------------------------------------------------------------------------------
 if __name__ == "__main__":
   print("cv2 version = " + str(cv2.__version__))
-  print("np version = " + str(np.__version__))
+  print("numpy version = " + str(np.__version__))
+  print("imutils version = " + str(imutils.__version__))
   print("MAIN STUFF")
+  # initialize weight for running average
+  aWeight = 0.5
 
+  # get the reference to the webcam
+  camera = cv2.VideoCapture(0)
+
+  # region of interest (ROI) coordinates
+  top, right, bottom, left = 10, 350, 225, 590
+
+  # initialize num of frames
+  num_frames = 0
+
+  # keep looping, until interrupted
   while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # get the current frame
+    (grabbed, frame) = camera.read()
 
-    # Our operations on the frame come here
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-      frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-      roi_gray = gray[y:y+h, x:x+w]
-      roi_color = frame[y:y+h, x:x+w]
-      eyes = eye_cascade.detectMultiScale(roi_gray)
-      for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # resize the frame
+    frame = imutils.resize(frame, width=700)
 
-    # display 
-    cv2.imshow('frame',frame)
+    # flip the frame so that it is not the mirror view
+    frame = cv2.flip(frame, 1)
 
-    # Display the gray frame
-    cv2.imshow('gray',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # clone the frame
+    clone = frame.copy()
+
+    # get the height and width of the frame
+    (height, width) = frame.shape[:2]
+
+    # get the ROI
+    roi = frame[top:bottom, right:left]
+
+    # convert the roi to grayscale and blur it
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    # to get the background, keep looking till a threshold is reached
+    # so that our running average model gets calibrated
+    if num_frames < 30:
+        run_avg(gray, aWeight)
+    else:
+      # segment the hand region
+      hand = segment(gray)
+
+      # check whether hand region is segmented
+      if hand is not None:
+        # if yes, unpack the thresholded image and
+        # segmented region
+        (thresholded, segmented) = hand
+
+        # draw the segmented region and display the frame
+        cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+        cv2.imshow("Thesholded", thresholded)
+
+    # draw the segmented hand
+    cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
+
+    # increment the number of frames
+    num_frames += 1
+
+    # display the frame with segmented hand
+    cv2.imshow("Video Feed", clone)
+
+    # observe the keypress by the user
+    keypress = cv2.waitKey(1) & 0xFF
+
+    # if the user pressed "q", then stop looping
+    if keypress == ord("q"):
       break
 
-  # cv2.imshow('img',img)
-  # cv2.waitKey(0)
-  cv2.destroyAllWindows()
+# free up memory
+camera.release()
+cv2.destroyAllWindows()
 
 # print("HEY THIS WORKS")
